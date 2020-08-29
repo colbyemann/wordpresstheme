bbd57f342ceca10f6fdd2d49d732290103d7b6f6
Colby Emann <hello@colbyemann.com>
2020-08-29 13:25:25 -0600

    Push Basic Layout

---
+++ front-page.php
@@ -0,0 +1,17 @@
+<?php get_header();?>
+
+<div class="container">
+    <div class="row">
+
+            <div class="col">
+                Left Side
+            </div>
+
+            <div class="col">
+                Right Side
+            </div>
+    </div>
+
+</div>
+
+<?php get_footer();?>
